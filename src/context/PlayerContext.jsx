2f444e8f2994e2bf0228e10bf9c1cbc76545f7e3
firaSE-16 <loarifse16@gmail.com>
2024-11-11 17:02:02 +0300

    Update PlayerContext.jsx

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "http://localhost:10000";
 
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
